9473ba030c75f034c2342d9dd40e499a92dcf987
David Trieu <davidtrieu1@gmail.com>
2023-07-28 19:44:42 -0500

    update

---
+++ script.js
@@ -0,0 +1,4 @@
+//https://api.twelvedata.com/time_series?symbol=BAC&interval=1day&apikey=3da1a21d5cbc4069af90a870929d0d80
+//	"message": "Invalid **interval** provided: . Supported intervals: 1min, 5min, 15min, 30min, 45min, 1h, 2h, 4h, 8h, 1day, 1week, 1month",
+//https://api.twelvedata.com/time_series?&start_date=2020-01-06&end_date=2020-05-06&symbol=aapl&interval=1day&apikey=3da1a21d5cbc4069af90a870929d0d80
+
